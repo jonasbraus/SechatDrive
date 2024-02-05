e51e2f3ea2c406d9e678b0d9d3000bd25718541b
brausjonas <brausjonas@gmail.com>
2024-02-05 10:20:28 +0100

    Update main.py

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ def page_login_credentials():
     token = login_handler.login_user(user)
 
     resp = Response()
-    resp.set_cookie("token", token)
+    resp.set_cookie("token", token, max_age=365*24*60*60)
     return resp
 
 
