4353a1f0ac1f73c7d4d2d78ff1c237ce95aeff14
brausjonas <brausjonas@gmail.com>
2024-02-08 21:10:48 +0100

    Update main.py

--- main.py
+++ main.py
@@ -350,7 +350,7 @@ def drive_get_share_folder_download():
         zip_file = f"./drive/{token}"
         if not os.path.exists(zip_file + ".zip"):
             shutil.make_archive(zip_file, "zip", f"{element}")
-            threading.Timer(5, delete_temp_zip, args=(zip_file + ".zip")).start()
+            threading.Timer(5, delete_temp_zip, args=(zip_file + ".zip",)).start()
         return send_file(f"./drive/{token}.zip", as_attachment=True)
     except:
         return "not valid"
