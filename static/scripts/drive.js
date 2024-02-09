d84e9c7df4da1994b9a9b3183d65ba4c02d2623a
Jonas Braus <jonas.braus@endress.com>
2024-02-09 12:14:06 +0100

    test

--- static/scripts/drive.js
+++ static/scripts/drive.js
@@ -197,7 +197,7 @@ async function delete_element(request_folder, element_name)
 function click_in_center(e)
 {
     document.querySelector(".addMenu").style.display = "none"
-    if (e.target.id !== "edit_x")
+    if (e.target.id !== "edit_x" && e.currentTarget.id !== "edit_x")
     {
         document.querySelector(".editMenu").style.display = "none"
     }
