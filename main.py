39de80a08897df2531ca453d441f91550d3ddd20
brausjonas <brausjonas@gmail.com>
2024-02-11 16:25:29 +0100

    Update main.py

--- main.py
+++ main.py
@@ -71,7 +71,7 @@ def page_drive():
     directory = sorted(directory)
 
     process = subprocess.run(["du", "-h", f"./drive/{user_id}"], capture_output=True, text=True)
-    size = process.stdout.split()[0]
+    size = process.stdout.split()[len(process.stdout.split()) - 1]
     print(size)
 
     return render_template("drive.html", directory=directory, request_folder=request_folder)
