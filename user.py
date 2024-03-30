4d412b27b431998b13750b67fb2653dd10ad5fa3
brausjonas <brausjonas@gmail.com>
2024-03-30 03:04:51 +0100

    moved

--- user.py
+++
@@ -1,7 +0,0 @@
-class User:
-    def __init__(self, user_id, first_name, last_name, mail, password):
-        self.user_id = user_id
-        self.first_name = first_name
-        self.last_name = last_name
-        self.mail = mail
-        self.password = password
